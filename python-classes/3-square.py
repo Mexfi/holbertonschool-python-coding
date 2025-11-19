0bf84b793e05a9a4a94cb20c28154a940c3554f5
Mexfi <cenab.1994@gmail.com>
2025-11-19 15:41:17 +0000

    asasad

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes the square with a given size"""
+        self.size = size  # setter is called
+
+    @property
+    def size(self):
+        """Getter for __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for __size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
